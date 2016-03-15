8b26c1fe2b65196c21e9b169da70df59df644b03
Joe Eagar <joeedh@gmail.com>
2016-03-15 00:33:21 -0700

    add these files

---
+++ kernel/drivers/blockdevice/cdrom.c
@@ -0,0 +1,11 @@
+#include "cdrom.h"
+
+#include "../../libc/libk.h"
+#include "../../libc/string.h"
+#include "../../libc/stdio.h"
+#include "../../task/task.h"
+#include "../../task/process.h"
+#include "../../task/task.h"
+#include "../../io.h"
+#include "../../timer.h"
+
---
+++ kernel/drivers/blockdevice/cdrom.h
@@ -0,0 +1,9 @@
+#ifndef _KCDROM_H
+#define _KCDROM_H
+
+#include <stddef.h>
+#include <stdint.h>
+
+#include "memblock.h"
+
+#endif /* _KCDROM_H */
